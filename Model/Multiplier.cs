05a7502f7beb22a6e85107b2c971b6f6da772fb1
Mateusz Arak <mateusz.arak@gmail.com>
2022-05-07 14:31:00 +0200

    add extension method

---
+++ Model/Multiplier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladiator.Model
+{
+    public static class Multiplier
+    {
+        public static double Extensions(this StatisticMultiplier statisticMultiplier)
+        {
+            switch (statisticMultiplier)
+            {
+                case StatisticMultiplier.Low:
+                    return 0.75;
+                case StatisticMultiplier.Medium:
+                    return 1.0;
+                case StatisticMultiplier.High:
+                    return 1.25;
+             
+            }
+            return 0.0;
+        }
+    }
+}
